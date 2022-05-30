7dbd7b3529133afc14ad470cb560a054f7531cd2
KhansaNandaTasya <97277262+KhansaNandaTasya@users.noreply.github.com>
2022-05-31 01:04:54 +0800

    Update routes.php

--- pages/routes.php
+++ pages/routes.php
@@ -10,25 +10,25 @@ if(isset($_GET['page'])){
             break;
         case 'karyawancreate':
             include "karyawan/karyawancreate.php";
-            break; 
+            break;
         case 'karyawanupdate':
             include "karyawan/karyawanupdate.php";
             break;
         case 'karyawandelete':
-            include "karyawan/karyawandelete";
+            include "karyawan/karyawandelete.php";
             break;
         case 'bagian':
             include "bagian/bagian.php";
-            break;  
-         case 'bagiancreate':
-                include "bagian/bagiancreate.php";
-             break;   
-         case 'bagianupdate':
-             include "bagian/bagianupdate.php";
-             break;  
-         case 'bagiandelete':
+            break;
+        case 'bagiancreate':
+            include "bagian/bagiancreate.php";
+            break;
+        case 'bagianupdate':
+            include "bagian/bagianupdate.php";
+            break;
+        case 'bagiandelete':
             include "bagian/bagiandelete.php";
-            break;             
+            break;
         default:
             include "dashboard.php";
             break;
